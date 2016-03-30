29284483da360e03b150e4014df958c3bd03dae8
Matt Jahns <msjahns88@gmail.com>
2016-03-30 13:42:08 -0400

    linked mongo into the server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,14 @@
+// express stuff
 var express = require( "express" );
 var notelyServerApp = express();
+
+// mongo stuff
+var db = require( "mongoose" );
+db.connect( "mongodb://localhost:27017/notely" );
+
+// test our mongo connection
+var NoteSchema = db.Schema( { title: String, body_html: String } );
+var Note = db.model( "Note", NoteSchema );
 
 // cross-origin resource sharing (CORS) middleware
 notelyServerApp.use( function ( req, res, next ) {
@@ -11,17 +20,10 @@ notelyServerApp.use( function ( req, res, next ) {
 });
 
 notelyServerApp.get( "/", function ( req, res ) {
-    // respond with json
-    res.json( [
-        {
-            title: "new hardcoded note",
-            body_html: "foo"
-        },
-        {
-            title: "another new hardcoded note",
-            body_html: "bar"
-        }
-    ]);
+    // respond with the note data
+    Note.find().then( function ( notes ) {
+        res.json( notes );
+    });
 });
 
 notelyServerApp.listen( 3030, function () {
