75d06d3cf7eaf1bb4d8fd087118259821c5134b5
Matt Jahns <msjahns88@gmail.com>
2016-03-31 11:37:29 -0400

    added updated_at property to notes

--- server/models/note-schema.js
+++ server/models/note-schema.js
@@ -1,8 +1,23 @@
 // mongo stuff
 var db = require ( "../config/db" );
-
+ 
 // schemas
-var NoteSchema = db.Schema( { title: String, body_html: String } );
+var NoteSchema = db.Schema( 
+    { 
+        title: String, 
+        body_html: String, 
+        update_at: { type: Date, default: Date.now }
+    } 
+);
+
+// define how mongo should act before saving a note
+NoteSchema.pre( "save", function ( next ) {
+    // we want to change the last date at which the note was updated
+    this.updated_at = Date.now();
+
+    // now go on to saving the note
+    next();
+});
 
 // export our schemas
 module.exports = NoteSchema;
